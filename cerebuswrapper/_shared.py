7b63d2fa550317603ddcb8938932890134986d93
Charles Guan <cguan@caltech.edu>
2020-11-11 17:46:17 -0800

    Remove singleton label from CbSdkConnection so more than one connection is possible

--- cerebuswrapper/_shared.py
+++
@@ -1,8 +0,0 @@
-def singleton(cls):
-    instances = {}
-
-    def getinstance(**kwargs):
-        if cls not in instances:
-            instances[cls] = cls(**kwargs)
-        return instances[cls]
-    return getinstance
